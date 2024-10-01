817b78b190a583520932a3a38440453b4b8c947d
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-10-01 12:16:05 +0000

    conversion of datatypes

--- js_basics/practice.js
+++ js_basics/practice.js
@@ -1,5 +1,3 @@
-const user_id=123456
-let user_name="shubham"
-
-console.log(user_id,user_name)
-console.table([user_id,user_name])
+let number = 2 
+let str = String(number)
+console.log(typeof str)
