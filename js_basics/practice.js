94993b19aaa8e19bdb689ea79f15296018c5cb75
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-10-04 13:16:19 +0000

    Date and Time in JS

--- js_basics/practice.js
+++ js_basics/practice.js
@@ -1,6 +1,27 @@
-const min=10, max=20
+// const min=10, max=20
 
-console.log(Math.floor(Math.random()*(max-min+1))+min) // Math.random() give elements between 0,1
+// console.log(Math.floor((Math.random()*(max-min+1))+min));
+//  // Math.random() give elements between 0,1
+
+
+
+// let myDate = new Date("01-24-2002")
+// let myTime = Date.now()
+
+// console.log(myTime);    // always return in mili seconds from 1st jan 1970
+// console.log(myDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)); //for converting mili seconds to seconds
+
+// let myDate = new Date()
+// // console.log(myDate);
+// // console.log(myDate.getMonth()+1);
+// // console.log(myDate.getDay());
+// console.log(myDate.toLocaleString('en-IN',{
+//     weekday: 'narrow'             // long return "Friday" Narrow returns "F"  short returns "Fri"
+// }));
+
+
 
 
 
