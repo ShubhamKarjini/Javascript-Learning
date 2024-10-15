e88e705204716acc380905323044c250b2c5514f
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-10-15 07:53:37 +0000

    ControlFlow

---
+++ 05_controlflow/one.js
@@ -0,0 +1,11 @@
+const temp = 45
+
+if(temp < 30){
+    console.log("temperature greater than 30");
+}else if (temp>40){
+    console.log("temperature greater than 40");
+}else{
+    console.log("temperature less than 50");
+}
+
+
