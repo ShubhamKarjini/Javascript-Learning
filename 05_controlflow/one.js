487ce68aca5d6e7655e8e78f3030b36edc7a0bde
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-10-16 09:01:41 +0000

    truthy and falsy

--- 05_controlflow/one.js
+++ 05_controlflow/one.js
@@ -1,11 +1,16 @@
-const temp = 45
+// const temp = 45
 
-if(temp < 30){
-    console.log("temperature greater than 30");
-}else if (temp>40){
-    console.log("temperature greater than 40");
-}else{
-    console.log("temperature less than 50");
-}
+// if(temp < 30){
+//     console.log("temperature greater than 30");
+// }else if (temp>40){
+//     console.log("temperature greater than 40");
+// }else{
+//     console.log("temperature less than 50");
+// }
+
+//Falsy values false, 0 , -0, BigInt, "", null, undefined, NaN
+// except these all are truthy values
+//truthy values "0","false"," ",[],{}, function(){}
+// nulling coalescing operator (??) = null, undefined
 
 
