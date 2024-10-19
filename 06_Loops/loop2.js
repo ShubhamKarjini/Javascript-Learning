1e6bb2b5b6af23ba09cc63c87b72bd58b1ac319f
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-10-19 14:27:14 +0000

    reduce()

--- 06_Loops/loop2.js
+++ 06_Loops/loop2.js
@@ -1,5 +1,28 @@
-const nums=[1,2,3,4,5,6,7,8]
-const newNums = nums.map( (item)=>{
-    return item + 10
-} )
+// using map
+// const nums=[1,2,3,4,5,6,7,8]
+// const newNums = nums.map( (item)=>{
+//     return item + 10
+// } )
+// console.log(newNums);
+
+// chaining
+
+// const nums=[1,2,3,4,5,6,7,8]
+
+// const newNums = nums
+//                     .map( (item)=> item * 10 )// item is updated with multiplication of 10
+//                     .map( (item)=> item +1 )// item is added up with 1
+// console.log(newNums);
+
+//REDUCE
+
+const nums = [1,2,3,4,5]
+const newNums = nums.reduce( (acc,currVal)=>{
+    return acc+currVal
+},0 )//0 represents initial value for accumulator
+
 console.log(newNums);
+
+
+
+
