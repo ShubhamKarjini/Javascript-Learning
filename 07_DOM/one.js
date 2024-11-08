aeac53ea8ee0e72efb5cc828f710f1bb408c248a
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-11-08 14:34:08 +0000

    DOM

--- 07_DOM/one.js
+++ 07_DOM/one.js
@@ -1,1 +1,5 @@
 // in js ".innerHTML" is used for element manipulation and ".getElementById" is used for accessing element
+// .querySelector always returns first appearance of the id/class
+//nodelist is different from array which doesn't have map() property
+//while using querySelectorAll it always returns a nodelist, so to access it we need to use index of the element we need to access.
+// getElementById returns HTML collection whereas queryselectorAll returns nodelist
