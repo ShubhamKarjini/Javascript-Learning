5963115b2df1900ccb577e81fdd78d46e982886d
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2024-11-09 16:02:20 +0000

    Dom

--- 07_DOM/one.js
+++ 07_DOM/one.js
@@ -4,3 +4,28 @@
 //while using querySelectorAll it always returns a nodelist, so to access it we need to use index of the element we need to access.
 // getElementById returns HTML collection whereas queryselectorAll returns nodelist
 //getElementByClassName always returns a HTMLcollection
+//if we wanted to access the nodeslist of some parent then it doesn't always return exact numbers, instead it includes text+next line element given 
+
+const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children[1].innerHTML);
+
+// for (let i = 0; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerHTML);
+    
+// }
+
+// console.log(parent.firstElementChild.innerHTML);
+// console.log(parent.lastElementChild.innerHTML);
+
+// const dayOne = document.querySelector('.day')
+// console.log(dayOne.innerHTML);
+// console.log(dayOne.parentElement.innerHTML);
+// console.log(dayOne.nextElementSibling.innerHTML);
+
+
+
+
+
+
+
